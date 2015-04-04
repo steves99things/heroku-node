8000a9a7652f48a88931297142cb33ef8ad0650c
Steve Carroll <scarroll531@gmail.com>
2015-04-04 16:12:16 -0400

    added variables

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+//global variables
+var company = "Rest Inc.";
+
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
@@ -11,11 +14,32 @@ app.set('view engine', 'ejs');
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-// set the home page route
+// set the routes
 app.get('/', function(req, res) {
-
+	var beds = [
+		{ name: 'King', comfort: 10 },
+		{ name: 'Queen', comfort: 8 },
+		{ name: 'Double', comfort: 6 },
+		{ name: 'Twin XL', comfort: 5 },
+		{ name: 'Twin', comfort: 4 }
+	];
+	var tagline = "You'll sleep the best with Rest!"
 	// ejs render automatically looks in the views folder
-	res.render('index');
+	res.render('pages/index', {
+		company: company,
+		beds: beds,
+		tagline: tagline
+	});
+});
+app.get('/about', function(req, res) {
+	res.render('pages/about', {
+		company: company
+	});
+});
+app.get('/contact', function(req, res) {
+	res.render('pages/contact', {
+		company: company
+	});
 });
 
 app.listen(port, function() {
@@ -24,9 +48,3 @@ app.listen(port, function() {
 
 // set a few other page routes to test
 
-app.get('/about', function(req, res) {
-	res.render('about');
-});
-app.get('/contact', function(req, res) {
-	res.render('contact');
-});
